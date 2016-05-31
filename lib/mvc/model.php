4eff17caf45cd2aeaa50f1875fcafec020581e2e
VladoVelkov <vlado.velkov@gmail.com>
2016-05-31 19:22:22 +0200

    Update model.php

--- lib/mvc/model.php
+++ lib/mvc/model.php
@@ -67,16 +67,16 @@ class Model {
 	protected $errors = [];
 
 	/**
-     * Instantiate the model object.
-     *
-     * @param array $params parameters used.
-	 * @param array $data data to work with
-	 * @param array $meta meta information about data
-	 * 
-	 * default table is named the same with the current module requested
-	 * default name for table identifier field is format table_id
-	 * default filter is by table_id 
-     */
+     	* Instantiate the model object.
+     	*
+     	* @param array $params parameters used.
+	* @param array $data data to work with
+	* @param array $meta meta information about data
+	* 
+	* default table is named the same with the current module requested
+	* default name for table identifier field is format table_id
+	* default filter is by table_id 
+     	*/
 	public function __construct($params,$data,$meta) {
 		$this->setParams($params);
 		$this->setData($data);
@@ -87,28 +87,28 @@ class Model {
 	}
 	
 	/**
-     * By default the model works with database. 
-     * If some model child class doesn't need database connection then override to return false.
-     * @return bool
-     */
+     	* By default the model works with database. 
+     	* If some model child class doesn't need database connection then override to return false.
+     	* @return bool
+     	*/
 	public function hasDatabase() {
 		return true;	
 	}
 	
 	/**
-     * Set PDO database connection. 
-	 *
-     * @param PDO $database connection to database.
-     */
+     	* Set PDO database connection. 
+	*
+     	* @param PDO $database connection to database.
+     	*/
 	public function setDatabase($database) {
 		$this->database = $database;
 	}
 	
 	/**
-     * Set meta array values. 
-	 *
-	 * @param array $src source array with meta information.
-     */
+     	* Set meta array values. 
+	*
+	* @param array $src source array with meta information.
+     	*/
 	public function setMeta($src) {
 		if(count($src)>0) {
 			foreach($src as $k=>$v) {
@@ -118,19 +118,19 @@ class Model {
 	}
 	
 	/**
-     * Get meta array values. 
-	 *
-	 * @return array model data.
-     */
+     	* Get meta array values. 
+	*
+	* @return array model data.
+     	*/
 	public function getData() {
 		return $this->data;
 	}
 	
 	/**
-     * Set data array values. 
-	 *
-	 * @param array $src source array with data to be appended to model data array.
-     */
+     	* Set data array values. 
+	*
+	* @param array $src source array with data to be appended to model data array.
+     	*/
 	public function setData($src) {
 		if(count($src)>0) {
 			$cnt = count($this->data);
@@ -141,28 +141,28 @@ class Model {
 	}
 	
 	/**
-     * Get parameter by name. 
-	 *
-	 * @return mixed parameter value or empty string if parameter not set.
-     */
+     	* Get parameter by name. 
+	*
+	* @return mixed parameter value or empty string if parameter not set.
+     	*/
 	public function getParam($p) {
 		return isset($this->params[$p]) ? $this->params[$p] : '';
 	}
 	
 	/**
-     * Get all parameters. 
-	 *
-	 * @return array model parameters.
-     */
+     	* Get all parameters. 
+	*
+	* @return array model parameters.
+     	*/
 	public function getParams() {
 		return $this->params;
 	}
 	
 	/**
-     * Set parameters array values. 
-	 *
-	 * @param array $src source array with parameters to be appended to model parameters array.
-     */
+     	* Set parameters array values. 
+	*
+	* @param array $src source array with parameters to be appended to model parameters array.
+     	*/
 	public function setParams($src) {
 		if(count($src)>0) {
 			foreach($src as $k=>$v) {
@@ -172,10 +172,10 @@ class Model {
 	}
 
 	/**
-     * Set filters array keys and values. 
-	 *
-	 * @param array $src source array with filters to be appended to model filters array.
-     */
+     	* Set filters array keys and values. 
+	*
+	* @param array $src source array with filters to be appended to model filters array.
+     	*/
 	public function setFilters($src) {
 		if(count($src)>0) {
 			foreach($src as $k=>$v) {
@@ -185,11 +185,11 @@ class Model {
 	}
 	
 	/**
-     * Set database tables the model database table belongs to. 
-	 *
-	 * @param number $i index of belongs array (there can be multiple parents)
-	 * @param array $src source array with belongs information to be appended to model belongs array.
-     */
+     	* Set database tables the model database table belongs to. 
+	*
+	* @param number $i index of belongs array (there can be multiple parents)
+	* @param array $src source array with belongs information to be appended to model belongs array.
+     	*/
 	public function setBelongs($i,$src) {
 		if(count($src)>0) {
 			foreach($src as $k=>$v) {
@@ -199,10 +199,10 @@ class Model {
 	}
 	
 	/**
-     * Set database tables the model database table is parent to. 
-	 *
-	 * @param array $src source array with hasmany information to be appended to model hasmany array.
-     */
+     	* Set database tables the model database table is parent to. 
+	*
+	* @param array $src source array with hasmany information to be appended to model hasmany array.
+     	*/
 	public function setHasMany($src) {
 		if(count($src)>0) {
 			foreach($src as $k=>$v) {
@@ -212,25 +212,25 @@ class Model {
 	}
 	
 	/**
-     * Get all errors. 
-	 *
-	 * @return array model errors.
-     */
+     	* Get all errors. 
+	*
+	* @return array model errors.
+     	*/
 	public function getErrors() {
 		return $this->errors;
 	}
 	
 	/**
-     * Prepare statement for every SQL execution in database.
-     * In case model database property was set to PDOException then set model errors.
-	 * If database connection set then bind values to filters named parameters for select/update/delete query conditions 
-	 * or bind values to data named parameters for insert/update queries.
-	 * Save debug and info messages in model errors array for later use.
-	 *
-     * @param string $sql SQL query to be executed with named params values to be binded to.
-	 * 	
-	 * @return PDOStatement prepared statement to be executed
-     */
+     	* Prepare statement for every SQL execution in database.
+     	* In case model database property was set to PDOException then set model errors.
+	* If database connection set then bind values to filters named parameters for select/update/delete query conditions 
+	* or bind values to data named parameters for insert/update queries.
+	* Save debug and info messages in model errors array for later use.
+	*
+     	* @param string $sql SQL query to be executed with named params values to be binded to.
+	* 	
+	* @return PDOStatement prepared statement to be executed
+     	*/
 	public function prepare($sql) {
 		$stmt = null;
 		if($this->database instanceof PDO) {
@@ -258,10 +258,10 @@ class Model {
 	}
 	
 	/**
-     * Count records in database that meets query conditions.
-     * Set model parameters for number of records, current page and number of pages.
-	 * Save debug and info messages in model errors array for later use.
-     */
+     	* Count records in database that meets query conditions.
+     	* Set model parameters for number of records, current page and number of pages.
+	* Save debug and info messages in model errors array for later use.
+     	*/
 	public function count() {
 		$stmt = $this->prepare($this->getCountSQL());
 		if(isset($stmt)) {
@@ -282,10 +282,10 @@ class Model {
 	}
 	
 	/**
-     * Select records from database that meets query conditions.
-     * Load records in model data array.
-	 * Save debug and info messages in model errors array for later use.
-     */
+     	* Select records from database that meets query conditions.
+     	* Load records in model data array.
+	* Save debug and info messages in model errors array for later use.
+     	*/
 	public function select() {
 		$stmt = $this->prepare($this->getSelectSQL());
 		if(isset($stmt)) {
@@ -304,11 +304,11 @@ class Model {
 	}
 
 	/**
-     * Insert record in database.
-	 * Save debug and info messages in model errors array for later use.
-	 *
-     * @return number last inserted id or 0 if query execution failed.
-     */
+     	* Insert record in database.
+	* Save debug and info messages in model errors array for later use.
+	*
+     	* @return number last inserted id or 0 if query execution failed.
+     	*/
 	public function insert() {
 		$stmt = $this->prepare($this->getInsertSQL());
 		if(isset($stmt)) {
@@ -325,11 +325,11 @@ class Model {
 	}
 	
 	/**
-     * Update record in database.
-	 * Save debug and info messages in model errors array for later use.
-	 *
-     * @return number rows affected or 0 if query execution failed.
-     */
+     	* Update record in database.
+	* Save debug and info messages in model errors array for later use.
+	*
+     	* @return number rows affected or 0 if query execution failed.
+     	*/
 	public function update() {
 		$stmt = $this->prepare($this->getUpdateSQL());
 		if(isset($stmt)) {
@@ -346,11 +346,11 @@ class Model {
 	}
 	
 	/**
-     * Delete record in database.
-	 * Save debug and info messages in model errors array for later use.
-	 *
-     * @return number rows affected or 0 if query execution failed.
-     */
+     	* Delete record in database.
+	* Save debug and info messages in model errors array for later use.
+	*
+     	* @return number rows affected or 0 if query execution failed.
+     	*/
 	public function delete() {
 		$stmt = $this->prepare($this->getDeleteSQL());
 		if(isset($stmt)) {
@@ -367,22 +367,22 @@ class Model {
 	}
 
 	/**
-     * Create SELECT COUNT query calling functions for select involved tables and defined conditions query parts.
-	 * 
-     * @return string $sqlcnt SQL query with named parameters.
-     */
+     	* Create SELECT COUNT query calling functions for select involved tables and defined conditions query parts.
+	* 
+     	* @return string $sqlcnt SQL query with named parameters.
+     	*/
 	protected function getCountSQL() {
 		$sqlcnt = "SELECT COUNT(DISTINCT ".$this->id.") FROM ".$this->getSelectTables()." ".$this->getSelectConditions();
 		return $sqlcnt;
 	}
 	
 	/**
-     * Create query to SELECT data from model database table and parent tables. 
-	 * For each record count child tables related records number.
-	 * Calling functions for select involved tables and defined conditions query parts
-	 *
-     * @return string $sql SQL query with named parameters.
-     */
+     	* Create query to SELECT data from model database table and parent tables. 
+	* For each record count child tables related records number.
+	* Calling functions for select involved tables and defined conditions query parts
+	*
+     	* @return string $sql SQL query with named parameters.
+     	*/
 	protected function getSelectSQL() {
 		$sql="SELECT ".$this->table.".*";
 		for($i=0,$cntb=count($this->belongs);$i<$cntb;$i++) {
@@ -403,11 +403,11 @@ class Model {
 	}
 	
 	/**
-     * Create query to INSERT data in model database table. 
-	 * List of fields to be inserted by default are those saved in model data array.
-	 *
-     * @return string $sql SQL query with named parameters for data values.
-     */
+     	* Create query to INSERT data in model database table. 
+	* List of fields to be inserted by default are those saved in model data array.
+	*
+     	* @return string $sql SQL query with named parameters for data values.
+     	*/
 	protected function getInsertSQL() {
 		$fields = array_keys($this->data[0]);
 		$sql = "INSERT INTO ".$this->table."(".join(",",$fields).") VALUES (:".join(",:",$fields).")";
@@ -415,11 +415,11 @@ class Model {
 	}
 	
 	/**
-     * Create query to UPDATE data in model database table and related tables. 
-	 * List of fields to be updated by default are those saved in model data array.
-	 *
-     * @return string $sql SQL query with named parameters for data values.
-     */
+     	* Create query to UPDATE data in model database table and related tables. 
+	* List of fields to be updated by default are those saved in model data array.
+	*
+     	* @return string $sql SQL query with named parameters for data values.
+     	*/
 	protected function getUpdateSQL() {
 		$fields = array_keys($this->data[0]);
 		$fieldsvalues = array_map(function($f){return $f.'=:'.$f;}, $fields);
@@ -428,20 +428,20 @@ class Model {
 	}
 	
 	/**
-     * Create query to DELETE data in model database table. 
-	 * Calling functions for select involved tables and defined conditions query parts
-	 *
-     * @return string SQL query with named parameters.
-     */
+     	* Create query to DELETE data in model database table. 
+	* Calling functions for select involved tables and defined conditions query parts
+	*
+     	* @return string SQL query with named parameters.
+     	*/
 	protected function getDeleteSQL() {
 		return "DELETE ".$this->table.".* FROM ".$this->getSelectTables()." ".$this->getSelectConditions();
 	}
 
 	/**
-     * Loop through all belongs array routes to define SQL query part with all joined tables. 
-	 *
-     * @return string JOIN part of the SQL query .
-     */
+     	* Loop through all belongs array routes to define SQL query part with all joined tables. 
+	*
+     	* @return string JOIN part of the SQL query .
+     	*/
 	protected function getSelectTables() {
 		$alltables = [$this->table];
 		$sql = $this->table." ";
@@ -458,10 +458,10 @@ class Model {
 	}
 	
 	/**
-     * Loop through all filters array elements to define SQL query conditions. 
-	 *
-     * @return string WHERE part of the SQL query .
-     */
+     	* Loop through all filters array elements to define SQL query conditions. 
+	*
+     	* @return string WHERE part of the SQL query .
+     	*/
 	protected function getSelectConditions() {
 		$cond = 'WHERE 1=1 ';
 		foreach ($this->params as $k=>$v) {
@@ -473,10 +473,10 @@ class Model {
 	}
 	
 	/**
-     * Loop through all params array elements to define SQL query part with sorting and paging. 
-	 *
-     * @return string part of the SQL query with sorting ang paging.
-     */
+     	* Loop through all params array elements to define SQL query part with sorting and paging. 
+	*
+     	* @return string part of the SQL query with sorting ang paging.
+     	*/
 	protected function getSelectParams() {
 		$sql_params = ' ';
 		if(in_array($this->params['sc'],range(1,20))) {
