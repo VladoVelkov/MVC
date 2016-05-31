9f0b62f8acccae87702e56a821d2e2fa12985e97
VladoVelkov <vlado.velkov@gmail.com>
2016-05-31 19:16:01 +0200

    Update cache.php

--- lib/mvc/cache.php
+++ lib/mvc/cache.php
@@ -19,21 +19,21 @@ class Cache {
 	protected $entry;
 	
 	/**
-     * Instantiate the cache object.
-     *
-     * @param string $storage path to the system cache storage.
-	 * @param array $params contains module, action, and other query string parameters
-     */
+     	* Instantiate the cache object.
+     	*
+     	* @param string $storage path to the system cache storage.
+	* @param array $params contains module, action, and other query string parameters
+	*/
 	public function __construct($storage,$params) {
 		$this->prefix = $storage.'/'.$params['module'];
 		$this->entry = md5(http_build_query($params));
 	}	
 	
 	/**
-     * Get content from cache file or empty string if not exists.
-     *
-     * @return string 
-     */
+     	* Get content from cache file or empty string if not exists.
+     	*
+     	* @return string 
+     	*/
 	public function get() {
 		if(is_file($this->prefix.'_'.$this->entry)) {
 			return file_get_contents($this->prefix.'_'.$this->entry);
@@ -42,17 +42,17 @@ class Cache {
 	}
 	
 	/**
-     * Save content to cache file.
-     *
-     * @return mixed number of characters saved or false if not successful 
-     */
+     	* Save content to cache file.
+     	*
+     	* @return mixed number of characters saved or false if not successful 
+     	*/
 	public function set($value) {
 		return file_put_contents($this->prefix.'_'.$this->entry,$value);
 	}
 	
 	/**
-     * Delete cache files with the same prefix
-     */
+     	* Delete cache files with the same prefix
+     	*/
 	public function invalidate() {
 		foreach(glob($this->prefix.'*') as $file) {
 			unlink($file);
