9d40ab8b6ed4bc6d8bdfa99569e6f6928aa66761
VladoVelkov <vlado.velkov@gmail.com>
2016-05-31 16:59:31 +0000

    Add files via upload

---
+++ example/app/model/products.php
@@ -0,0 +1,24 @@
+<?php
+class ProductsModel extends Model {
+	
+	public function __construct($params,$data,$meta) {
+		
+		parent::__construct($params,$data,$meta);
+		
+		$this->belongs[0] = [
+			'categories'=>['products_categories_id=categories_id','categories_name']
+		];
+		
+		$this->filters = [
+			'id' => 'products_id = :id',
+			'cid' => 'products_categories_id = :cid'
+		];
+		
+		$this->types = [
+			'id' => PDO::PARAM_INT,
+			'cid' => PDO::PARAM_INT
+		]	
+	}
+	
+}
+?>
