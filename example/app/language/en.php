379295981abdd1d801bb8b4af620b04fcbeb77c5
VladoVelkov <vlado.velkov@gmail.com>
2016-05-31 19:00:26 +0200

    Create en.php

---
+++ example/app/language/en.php
@@ -0,0 +1,18 @@
+<?php
+return [
+	'STATUS_NOT_CONNECTED'		=> 'Can not connect to database',
+	'STATUS_NOT_PREPARED'		=> 'Can not connect to database',
+	'STATUS_NOT_SELECTED'		=> 'Data can not be selected from database',
+	'STATUS_NOT_INSERTED'		=> 'Data can not be inserted in database.',
+	'STATUS_NOT_UPDATED'		=> 'Data can not be updated in database',
+	'STATUS_NOT_DELETED'		=> 'Data can not be deleted in database',
+	
+	'VALID_CATEGORY' 			=> 'Category Name is required, format letters only',	
+	'VALID_PRICE_AMOUNT'		=> 'Price Amount is required, format decimal',			
+	'VALID_PRICE_PRODUCT'		=> 'Price Product is required',						
+	'VALID_PRICE_QUANTITY'		=> 'Price Quantity is required, format numbers only',	
+	'VALID_PRODUCT'				=> 'Product Name is required',							
+	'VALID_PRODUCT_CATEGORY'	=> 'Product Category is required',						
+	'VALID_PRODUCT_DESCRIPTION'	=> 'Product Description is required',					
+];
+?>
